30dbe2ce0682e03f3b65e5aa630889131c99bb8f
ctznOScraft <yuyiui60@gmail.com>
2025-03-02 00:16:50 +0300

    Added test func - print-data.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Task_Tracker
 Task Tracker is a CLI app for managing tasks.
+
+To run this program install .NET
--- Task_Tracker/Program.cs
+++ Task_Tracker/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿namespace Task_Tracker {
+    static class Program {
+        private static void PrintData(string[] data) {
+            for (int i = 0; i < data.Length; i++) {
+                Console.Write(data[i]);
+                if (i < data.Length - 1) {
+                    Console.Write(" ");
+                }
+                else if (i == data.Length - 1) {
+                    Console.Write("\n");
+                }
+            }
+        }
+        static void Main(string[] args) {
+            if (args.Length > 0 && args[0] == "print-data") {
+                if (args.Length > 1) {
+                    PrintData(args[1..]);
+                }
+                else {
+                    Console.WriteLine("No data provided to print.");
+                }
+            }
+            else {
+                Console.WriteLine("Usage: Task_Tracker print-data [data to print]");
+            }
+        }
+    }
+}
